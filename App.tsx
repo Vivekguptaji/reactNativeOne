0fafc0ed1f5c4c6110dcad5f0768938e3a102187
Vivek <vivekknit786@gmail.com>
2022-10-15 13:57:22 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  return (
+    <WebView
+      source={{
+        uri: 'https://www.vshowcards.com',
+      }}
+    />
+  );
+};
+
+export default App;
